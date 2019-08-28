06f776eab04ed066c9ae09580c0505f9e62a0674
LuisPa <diganluispa@gmail.com>
2019-08-27 21:37:07 -0600

    function update

--- main.go
+++ main.go
@@ -7,12 +7,11 @@ import (
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       "Hello AWS Lambda and Netlify",
+o		StatusCode: 200,
+		Body:       "Hola desde una lambda escrita en go!",
 	}, nil
 }
 
 func main() {
-	// Make the handler available for Remote Procedure Call by AWS Lambda
 	lambda.Start(handler)
 }
